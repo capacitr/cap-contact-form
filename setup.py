4e016eed9ca422d96a789922b4a2a3e5b2099723
patrick aubin <paubin12@gmail.com>
2013-02-22 15:36:06 -0600

    renamed package

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.1.0',
     author='Patrick Aubin',
     author_email='patrick@capacitr.com',
-    packages=['contact_form'],
+    packages=['cap_contact_form'],
     url='http://capacitr.com/',
     description='A simple django-based contact form.',
 )
