1c003dc13eda55e2d95f266f75fda41721a0a9de
patrick aubin <paubin12@gmail.com>
2013-02-26 12:56:27 -0600

    updated the models and views

--- cap_contact_form/models.py
+++ cap_contact_form/models.py
@@ -1,3 +1,85 @@
 from django.db import models
 
-# Create your models here.
+SOURCE_CHOICES = (
+    ('quick-contact', 'quick-contact'),
+    ('dedicated-contact', 'dedicated-contact'),
+    ('blog-post', 'blog-post'),
+    ('referral', 'referral')
+    )
+
+class Contact(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+
+    address = models.TextField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=2)
+    zip_code = models.CharField(max_length=255)
+    phone = models.CharField(max_length=255, blank=True, null=True)
+
+    email = models.CharField(max_length=255, blank=True, null=True)
+    message = models.TextField(blank=True, null=True)
+
+    source = models.CharField(max_length=255, choices=SOURCE_CHOICES, blank=True, default="dedicated-contact")
+#    procedure = models.CharField(max_length=255)
+#    procedures_long = models.CharField(max_length=255, blank=True, null=True)
+#
+#    contact_by = models.CharField(max_length=255, choices=CONTACT_BY)
+#    best_call_time = models.DateTimeField(blank=True)
+#    hear_about_us = models.CharField(max_length=255, default="", blank=True)
+#    message = models.TextField()
+#
+#    date_contacted = models.DateTimeField(null=True, blank=True)
+#    call_message = models.TextField(blank=True)
+#    source = models.CharField(max_length=255, choices=SOURCE_CHOICES, blank=True, default="dedicated-contact")
+#
+#    location = models.CharField(max_length=255, blank=True)
+#
+
+class Attribute(models.Model):
+    post = models.ForeignKey('cap_contact_form.Contact')
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+    value = models.CharField(max_length=255)
+
+    class Meta:
+        unique_together = ('post', 'slug')
+
+
+FORM_TEMPLATES = (
+    ("embed_basic.html", "Basic Contact Form"),
+    ("embed_promo_holiday.html", "Trivia Form"),
+)
+
+class Form(DefaultModel):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    form_template = models.CharField(max_length=255, choices=FORM_TEMPLATES)
+
+    message = models.TextField(max_length=255, blank=True, default="")
+    thank_you_message = models.TextField(blank=True)
+    form_type = models.CharField(max_length=255, choices=SOURCE_CHOICES, default="blog-post")
+
+    @property
+    def embed_code(self):
+        code = ""
+        try:
+            sizes = {
+                "embed_basic.html" : (740, 250),
+                "embed_promo_holiday.html" : (740, 250),
+            }
+            code = """<iframe style="border: none; overflow: hidden;" src="http://www.drdayan.com%s" frameborder="0" scrolling="no" width="%s" height="%s"></iframe>""" % (self.get_absolute_url(), sizes[self.form_template][0], sizes[self.form_template][1])
+        except KeyError:
+            pass
+
+        return code 
+
+    def __unicode__(self):
+        return "%s" % self.name
+
+    @models.permalink
+    def get_absolute_url(self):
+        return ("drdayan.views.get_form", (), {
+            "form_slug" : self.slug 
+        })
+
--- cap_contact_form/views.py
+++ cap_contact_form/views.py
@@ -19,16 +19,24 @@ class ContactUsView(TemplateView):
 
             to = settings.CONTACT_EMAILS
 
-            name = contact_form.cleaned_data.get("name", None)
+            name = contact_form.cleaned_data.get("name", "newsletter")
             from_email = contact_form.cleaned_data.get("from_email", None)
-            message = contact_form.cleaned_data.get("message", None)
+            message = contact_form.cleaned_data.get("message", "sign me up for the newsletter")
 
             message = render_to_string('email.html',  {
                 'message' : message, 
                 'name' : name
             })
 
+            message = name + " says\n\r\n\r" + message 
+
             send_mail('Contact Form', message, from_email, to)
+
+            contact = models.Contact()
+            contact.name = name
+            contact.email = from_email
+            contact.message = message
+            contact.save()
 
             messages.add_message(self.request, messages.INFO, 'Your email has been sent.')
 
@@ -36,4 +44,208 @@ class ContactUsView(TemplateView):
             messages.add_message(self.request, messages.INFO, 'You didn\'t fill out the form correctly.')
 
         return redirect(reverse('contact_us'))
+
+def get_form(request, form_slug):
+    form = get_object_or_404(Form, slug=form_slug)
+
+    c = {
+        "slug" : form.slug,
+        "message" : form.message,
+        "source" : form.form_type
+        }
+    return render(request, form.form_template, c)
+
+def thank_you(req):
+    return direct_to_template(req, 'thank-you.html')
+
+def thank_you_qc(req):
+    return direct_to_template(req, 'thank-you-qc.html')
+
+@csrf_exempt
+def contact(req):
+    if req.method == 'POST':
+        procedures = req.POST.getlist('procedures')
+
+        # honeypot
+        if req.POST.get('username', None) or req.POST.get('name', None) != "Your full name":
+            return redirect(reverse('thank_you'))
+
+        source = req.POST.get('s', None)
+
+        name = "%s %s" % (req.POST.get('first_name', ""), req.POST.get('last_name', ""),)
+
+        if not name.strip():
+            return redirect(reverse('thank_you'))
+
+        c = {
+            'name' : name, #req.POST.get('first_name'),
+            'phone' : req.POST.get('phone_number'),
+            'email' : req.POST.get('email'),
+            'procedure' : req.POST.get('procedure', ''),
+            'procedures' : ", ".join(req.POST.getlist('procedures')),
+            'message' : req.POST.get('message'),
+            'hear_about_us' : req.POST.get('hear_about_us', ''),
+            'address' : req.POST.get('address', ""),
+            'location' : req.POST.get("l", ""),
+        }
+
+        form_slug = req.GET.get("e")
+        form = None
+
+        # append message to existing message if it exists
+
+        try:
+            form = Form.objects.get(slug=form_slug)
+            if form.message:
+                c["message"] = "%s - %s" % (form.message, c["message"],)
+        except Form.DoesNotExist:
+            pass
+
+        ct = Contact()
+
+        try:
+            ct.user = User.objects.get(username='admin')
+            ct.updated_by = User.objects.get(username='admin')
+
+            ct.name = name
+            ct.phone = c['phone']
+            ct.email = c['email']
+            ct.message = c['message']
+            ct.address = c['address']
+            ct.location = req.POST.get("l", "") 
+            ct.procedure = c['procedure']
+            ct.procedures_long = c['procedures']
+            ct.hear_about_us = c['hear_about_us']
+            ct.best_call_time = datetime.datetime.now()
+
+            ct.source = source
+            ct.save()
+
+            ct.site = [Site.objects.get(pk__exact=settings.SITE_ID)]
+        except User.DoesNotExist, Site.DoesNotExist:
+            pass
+
+        email_body = render_to_string('email.html', c)
+
+        try:
+            site_config = SiteConfig.objects.get(site__pk__exact=settings.SITE_ID)
+            emails = Email.objects.filter(site_config=site_config)
+
+            send_mail('[Dr Dayan] New contact', email_body, c['email'], emails.values_list('email_address', flat=True)) 
+            
+            from django.core.mail import EmailMultiAlternatives
+
+            subject, from_email, to = 'The practice of Steven H. Dayan, MD, FACS thanks you for your contact.', 'MyClinicalTeam@drdayan.com', c["email"]
+
+            text_content = render_to_string("email_response.html", {"name" : name})
+            html_content = text_content
+
+            msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+            msg.attach_alternative(html_content, "text/html")
+            msg.send()
+
+        except SiteConfig.DoesNotExist:
+            pass
+
+         
+        # show custom thank you message if form is custom
+        if form:
+            return render(req, "thank_you_message.html", {"message" : form.thank_you_message })
+
+#            if  == "promo3":
+#                c["message"] = "Add me to Holiday Card List - " + c["message"] 
+#
+#    if req.GET.get("e") in ["promo", "promo2"]:
+#        return render(req, "promo_message.html") 
+#
+
+    if "quick-contact" in source:
+        return redirect(reverse('thank_you_qc'))
+
+    return redirect(reverse('thank_you'))
+
+from django.template.loader import render_to_string
+
+@csrf_exempt
+def referral(req):
+    message = ""
+
+    if req.method == 'POST':
+
+        if req.POST.get('username', None):
+            return redirect(reverse('thank_you'))
+
+        source = req.POST.get('s', None)
+        if source:
+            if source == 'q':
+                source = 'quick-contact'
+            elif source == 'd':
+                source = 'dedicated-contact'
+            elif source == 'b':
+                source = 'blog-post'
+            elif source == 'r':
+                source = 'referral'
+
+
+
+        c = {
+            'name' : req.POST.get('name'),
+            'phone' : req.POST.get('phone'),
+            'email' : req.POST.get('email'),
+            'friends_name' : req.POST.get('friends_name'),
+            'friends_email' : req.POST.get('friends_email'),
+        }
+
+        ct = Contact()
+
+
+        try:
+            ct.user = User.objects.get(username='admin')
+            ct.updated_by = User.objects.get(username='admin')
+
+            ct.name = c['name']
+            ct.phone = c['phone']
+            ct.email = c['email']
+            ct.message = 'Promotion' 
+            ct.source = source
+            ct.best_call_time = datetime.datetime.now()
+
+            ct.save()
+
+            ct.site = [Site.objects.get(pk__exact=settings.SITE_ID)]
+        except User.DoesNotExist, Site.DoesNotExist:
+            pass
+
+        ct = Contact()
+        try:
+            ct.user = User.objects.get(username='admin')
+            ct.updated_by = User.objects.get(username='admin')
+
+            ct.name = c['friends_name']
+            ct.email = c['friends_email']
+            ct.message = 'Promotion' 
+            ct.source = source
+            ct.best_call_time = datetime.datetime.now()
+
+            ct.save()
+
+            ct.site = [Site.objects.get(pk__exact=settings.SITE_ID)]
+        except User.DoesNotExist, Site.DoesNotExist:
+            pass
+
+        email_body = render_to_string('promotion_email.html', c)
+
+        try:
+            site_config = SiteConfig.objects.get(site__pk__exact=settings.SITE_ID)
+            msg = EmailMultiAlternatives('[Dr Dayan] Promotion', email_body, c['email'], [c['friends_email']])
+            msg.attach_alternative(email_body, "text/html")
+            msg.send()
+
+        except SiteConfig.DoesNotExist:
+            pass
+
+        message = render_to_string('embed_message.html',  {'message' : message})
+
+    return HttpResponse(message) #redirect(reverse('thank_you'))
+
 
